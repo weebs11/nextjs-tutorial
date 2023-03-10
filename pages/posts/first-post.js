ae0030e043feed11b23947084b3043609adb1946
Kelly Wehbi <kwehbi@gmail.com>
2023-03-10 15:58:45 -0600

    adding all source control

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Layout from '../../components/layout';
-import styles from '../../styles/Home.module.css';
 
 export default function FirstPost() {
   return (
